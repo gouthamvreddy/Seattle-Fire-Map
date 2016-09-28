8014db84e496a12ae53b47a8a3cf8326efb22ffd
Goutham Reddy <gouthamvreddy@gmail.com>
2016-09-28 15:39:55 -0700

    created React template app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {PropTypes} from 'react';
+import styles from './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        Hello World!
+      </main>
+    );
+  }
+}
+
+export default App;
