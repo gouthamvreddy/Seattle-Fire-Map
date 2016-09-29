f42d6b388a4d062c71697a4504a30a0fbb774d41
Goutham Reddy <gouthamvreddy@gmail.com>
2016-09-28 19:13:16 -0700

    updated Map component with styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React, {PropTypes} from 'react';
 import styles from './App.css';
+import GMap from './Map';
 
 class App extends React.Component {
   render() {
     return (
       <main>
-        Hello World!
+       <GMap initialCenter={initialCenter} />
       </main>
     );
   }
 }
+
+let initialCenter = {lng: -122.303345, lat: 47.610366};
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,86 @@
 import React, {Component} from 'react';
+import styles from './Map.css';
 
-export default class Map extends Component {
-  render(){
-    <div className='map'></div>
+export default class GMap extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {zoom: 10};
+  }
+
+  static propTypes() {
+  	initialCenter: React.PropTypes.objectOf(React.PropTypes.number).isRequired
+  }
+
+	render() {
+    return (
+      <div className={styles.GMap}>
+        <div className={styles.UpdatedText}>
+          <p>Current Zoom: { this.state.zoom }</p>
+        </div>
+        <div className={styles.GMapCanvas} ref="mapCanvas"></div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    // create the map, marker and infoWindow after the component has
+    // been rendered because we need to manipulate the DOM for Google =(
+    this.map = this.createMap()
+    this.marker = this.createMarker()
+    this.infoWindow = this.createInfoWindow()
+
+    $.ajax({
+      method: "GET",
+      url: "https://data.seattle.gov/resource/grwu-wqtk.json?$limit=10",
+      cache: false
+    }).then((res) => {
+      console.log(res);
+    });
+    // have to define google maps event listeners here too
+    // because we can't add listeners on the map until its created
+    google.maps.event.addListener(this.map, 'zoom_changed', ()=> this.handleZoomChange())
+  }
+
+  // clean up event listeners when component unmounts
+  componentDidUnMount() {
+    google.maps.event.clearListeners(map, 'zoom_changed')
+  }
+
+  createMap() {
+    let mapOptions = {
+      zoom: this.state.zoom,
+      center: this.mapCenter()
+    }
+    return new google.maps.Map(this.refs.mapCanvas, mapOptions)
+  }
+
+  mapCenter() {
+    return new google.maps.LatLng(
+      this.props.initialCenter.lat,
+      this.props.initialCenter.lng
+    )
+  }
+
+  createMarker() {
+    return new google.maps.Marker({
+      position: this.mapCenter(),
+      map: this.map
+    })
+	}
+
+  createInfoWindow() {
+    let contentString = "<div class='InfoWindow'>I'm a Window that contains Info Yay</div>"
+    return new google.maps.InfoWindow({
+      map: this.map,
+      anchor: this.marker,
+      content: contentString
+    })
+  }
+
+  handleZoomChange() {
+    this.setState({
+      zoom: this.map.getZoom()
+    })
   }
 }
